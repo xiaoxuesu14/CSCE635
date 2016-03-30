0f89b1ec32d3cfef7672abe6a458c298a16823e7
Tim <twoodbury@tamu.edu>
2016-03-30 12:52:54 -0500

    Added python scripts for processing data. circleFit fits assuming constant-radius turns between points and works OK for estimating minimum radius of curvature. curveFit.py tries to use splines to compute derivatives and get the curvature that way, but the data are too noisy.

---
+++ python/gpsParser/circleFit.py
@@ -0,0 +1,110 @@
+import sys
+import numpy as np
+import math
+
+import gpsParser
+
+import scipy.interpolate
+import matplotlib.pyplot as plt
+import scipy.optimize
+
+def circleFit(x,x1,x2,x3):
+    R = x[2]
+    x0 = x[0]
+    y0 = x[1]
+    f1 = math.pow(x1[0]-x0,2.0) + math.pow(x1[1]-y0,2.0) - math.pow(R,2.0)
+    f2 = math.pow(x2[0]-x0,2.0) + math.pow(x2[1]-y0,2.0) - math.pow(R,2.0)
+    f3 = math.pow(x3[0]-x0,2.0) + math.pow(x3[1]-y0,2.0) - math.pow(R,2.0)
+    #f = f1*f1+f2*f2+f3*f3
+    f = np.array([f1,f2,f3])
+    return f
+
+#file = '20160328172944.csv'
+file = '20160328172331.csv'
+dat = gpsParser.parseFile(file)
+
+# shorten to relevant interval
+if file == '20160328172944.csv':
+    k1 = np.nonzero((dat[:,0]-dat[0,0] > 322))
+    k2 = np.nonzero((dat[:,0]-dat[0,0] < 375))
+    kp = np.intersect1d(k1[0],k2[0])
+    dat = dat[kp,:]
+if file == '20160328172331.csv':
+    k1 = np.nonzero((dat[:,0]-dat[0,0] > 170))
+    k2 = np.nonzero((dat[:,0]-dat[0,0] < 300))
+    kp = np.intersect1d(k1[0],k2[0])
+    dat = dat[kp,:]
+
+t = dat[:,0]-dat[0,0]
+vel = dat[:,5]
+x = dat[:,6]-dat[0,6]
+y = dat[:,7]-dat[0,7]
+
+xSol = np.zeros((len(t),3))
+for k in range(1,len(t)-1):
+    x0 = np.array([0.0,0.0,1.0])
+    x1 = np.array([x[k-1],y[k-1]])
+    x2 = np.array([x[k],y[k]])
+    x3 = np.array([x[k+1],y[k+1]])
+    xs = scipy.optimize.fsolve(circleFit,x0,args=(x1,x2,x3,))
+    #print(k,xs)
+    xSol[k,:] = xs
+
+# interpolate for plotting
+desn = 50
+xyhr = np.zeros(((len(t)-1)*desn,2))
+for k in range(1,len(t)-1):
+    th2 = math.atan2(y[k+1]-xSol[k,1],x[k+1]-xSol[k,0])
+    th1 = math.atan2(y[k-1]-xSol[k,1],x[k-1]-xSol[k,0])
+    th = np.linspace(th1,th2,desn)
+    #th = np.linspace(0,2.0*math.pi,desn)
+    xyhr[ (k*desn):((k+1)*desn),0 ] = xSol[k,2]*np.cos(th) + xSol[k,0]
+    xyhr[ (k*desn):((k+1)*desn),1 ] = xSol[k,2]*np.sin(th) + xSol[k,1]
+
+# simple curve fit for turning radius(vel):
+# bin velocities every m/s
+bins = [2,4,6,8]
+minr = [1000.0,1000.0,1000.0,1000.0]
+for k in range(len(vel)):
+    for j in range(len(bins)):
+        #print(k,j,vel[k],math.fabs(xSol[k,2]),bins[j])
+        if vel[k] < bins[j]:
+            rad = math.fabs(xSol[k,2])
+            if minr[j] > rad and rad > 1e-2:# tolerance for reasonableness
+                minr[j] = rad
+            break
+print(bins,minr)
+# compute the best-fit
+A = np.zeros((len(bins),2))
+Yf = np.zeros((len(bins),))
+for k in range(len(bins)):
+    A[k,0] = float(bins[k])-1.0
+    A[k,1] = 1.0
+    Yf[k] = math.log10(minr[k])
+Xf = np.dot(np.dot(np.linalg.inv( np.dot(A.transpose(),A)),A.transpose()),Yf)
+print('Y = %g*log10(radius)+%g' % (Xf[0],Xf[1]))
+Yp = np.dot(A,Xf)
+Ype = np.power(10.0,Yp)
+print('Y_pred = %g,%g,%g,%g' % (Ype[0],Ype[1],Ype[2],Ype[3]))
+xe = np.linspace(1.0,9.0,10)
+
+fig = plt.figure()
+ax = []
+ax.append( fig.add_subplot(3,1,1,ylabel='radius(m)') )
+ax[0].plot(t,np.abs(xSol[:,2]))
+ax[0].grid()
+#ax.append( fig.add_subplot(3,1,2,ylabel='y(m)',xlabel='x(m)') )
+#ax[1].plot(x,y,'b-')
+#ax[1].plot(xyhr[:,0],xyhr[:,1],'r--')
+ax.append( fig.add_subplot(3,1,2,ylabel='log10(radius)(m)',xlabel='V(m/s)') )
+ax[1].plot(vel,np.log10(np.abs(xSol[:,2])),'bx')
+# plot curve fit
+ax[1].plot(xe,xe*Xf[0]+Xf[1],'r-')
+ax[1].grid()
+ax.append( fig.add_subplot(3,1,3,ylabel='V(m/s)') )
+ax[2].plot(t,vel)
+ax[2].grid()
+fig.show()
+
+raw_input('Return to exit')
+plt.close(fig)
---
+++ python/gpsParser/curveFit.py
@@ -0,0 +1,96 @@
+import sys
+import numpy as np
+
+import gpsParser
+
+import scipy.interpolate
+import matplotlib.pyplot as plt
+
+#file = '20160328172944.csv'
+file = '20160328172331.csv'
+dat = gpsParser.parseFile(file)
+
+# shorten to relevant interval
+if file == '20160328172944.csv':
+    k1 = np.nonzero((dat[:,0]-dat[0,0] > 322))
+    k2 = np.nonzero((dat[:,0]-dat[0,0] < 375))
+    kp = np.intersect1d(k1[0],k2[0])
+    dat = dat[kp,:]
+if file == '20160328172331.csv':
+    k1 = np.nonzero((dat[:,0]-dat[0,0] > 170))
+    k2 = np.nonzero((dat[:,0]-dat[0,0] < 300))
+    kp = np.intersect1d(k1[0],k2[0])
+    dat = dat[kp,:]
+
+t = dat[:,0]-dat[0,0]
+x = dat[:,6]-dat[0,6]
+y = dat[:,7]-dat[0,7]
+
+ti = np.linspace(t[0],t[-1],10*len(t))
+
+xtck = scipy.interpolate.splrep(t,x,k=3,s=1.0)
+ytck = scipy.interpolate.splrep(t,y,k=3,s=1.0)
+
+# evaluate the spatial derivatives
+xs = scipy.interpolate.splev(ti,xtck,der=0)
+ys = scipy.interpolate.splev(ti,ytck,der=0)
+vx = scipy.interpolate.splev(ti,xtck,der=1)
+vy = scipy.interpolate.splev(ti,ytck,der=1)
+ax = scipy.interpolate.splev(ti,xtck,der=2)
+ay = scipy.interpolate.splev(ti,ytck,der=2)
+'''
+vxu = scipy.interpolate.splev(t,xtck,der=1)
+vyu = scipy.interpolate.splev(t,ytck,der=1)
+# curve fit to the velocity
+vxtck = scipy.interpolate.splrep(t,vxu,k=3,s=1.0)
+vytck = scipy.interpolate.splrep(t,vyu,k=3,s=1.0)
+vxs = scipy.interpolate.splev(ti,vxtck,der=0)
+vys = scipy.interpolate.splev(ti,vytck,der=0)
+ax = scipy.interpolate.splev(ti,vxtck,der=1)
+ay = scipy.interpolate.splev(ti,vytck,der=1)
+'''
+
+# calculate curvature
+K = np.zeros(len(ti),)
+# radius of curvature
+R = np.zeros(len(ti),)
+for k in range(len(ti)):
+    K[k] = (vx[k]*ay[k]-vy[k]*ax[k])/np.power(vx[k]*vx[k]+vy[k]*vy[k],1.5)
+    R[k] = 1.0/K[k]
+
+mp = plt.figure()
+axu = mp.add_subplot(1,1,1)
+axu.plot(x,y)
+axu.plot(xs,ys,'r--')
+mp.show()
+
+# plot
+fig = plt.figure()
+axi = []
+lbls = ['x(m)','y(m)','vx(m/s)','vy(m/s)','ax(m/s2)','ay(m/s2)','curvature (m^-1)','radius of curvature (m)']
+for k in range(8):
+    axi.append(fig.add_subplot(4,2,k+1,ylabel=lbls[k]))
+    if k == 0:
+        axi[k].plot(t,x)
+        axi[k].plot(ti,xs,'r--')
+    elif k == 1:
+        axi[k].plot(t,y)
+        axi[k].plot(ti,ys,'r--')
+    elif k == 2:
+        axi[k].plot(ti,vx)
+        #axi[k].plot(ti,vxs,'r--')
+    elif k == 3:
+        axi[k].plot(ti,vy)
+        #axi[k].plot(ti,vys,'r--')
+    elif k == 4:
+        axi[k].plot(ti,ax)
+    elif k == 5:
+        axi[k].plot(ti,ay)
+    elif k == 6:
+        axi[k].plot(ti,np.abs(K))
+    elif k == 7:
+        axi[k].plot(ti,np.abs(R))
+fig.show()
+raw_input('Return to continue')
+plt.close(fig);
+plt.close(mp);
